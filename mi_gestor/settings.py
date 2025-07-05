455c018d1db0399456cb58a9b63be816168aea98
jhonmaster14 <jhon_zuloeta@hotmail.com>
2025-07-05 16:09:48 -0500

    configuracion

--- mi_gestor/settings.py
+++ mi_gestor/settings.py
@@ -11,17 +11,27 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
-from decouple import config
+from decouple import config, UndefinedValueError
+from django.core.management.utils import get_random_secret_key
+import os
+
+ 
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+env_path = BASE_DIR / ".env"
+
+# Obtener o generar SECRET_KEY automáticamente
+try:
+    SECRET_KEY = config("SECRET_KEY")
+except UndefinedValueError:
+    SECRET_KEY = get_random_secret_key()
+    with open(env_path, "a") as f:
+        f.write(f"\nSECRET_KEY={SECRET_KEY}")
 
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config('SECRET_KEY')
+ 
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG =  config('DEBUG', default=False, cast=bool)
