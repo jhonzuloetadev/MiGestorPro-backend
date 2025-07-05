d853b1de245c169dad4159e56adeebc256435092
jhonmaster14 <jhon_zuloeta@hotmail.com>
2025-07-05 16:22:20 -0500

    bd sqlite

--- mi_gestor/settings.py
+++ mi_gestor/settings.py
@@ -87,18 +87,18 @@ WSGI_APPLICATION = 'mi_gestor.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.sqlite3',
-    #     'NAME': BASE_DIR / 'db.sqlite3',
-    # }
     'default': {
-        'ENGINE': 'django.db.backends.mysql', 
-        'NAME': config('DB_NAME'),
-        'USER': config('DB_USER'),
-        'PASSWORD': config('DB_PASSWORD'),
-        'HOST': config('DB_HOST', default='127.0.0.1'),
-        'PORT': config('DB_PORT', default='3306'),
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.mysql', 
+    #     'NAME': config('DB_NAME'),
+    #     'USER': config('DB_USER'),
+    #     'PASSWORD': config('DB_PASSWORD'),
+    #     'HOST': config('DB_HOST', default='127.0.0.1'),
+    #     'PORT': config('DB_PORT', default='3306'),
+    # }
 }
 
 
